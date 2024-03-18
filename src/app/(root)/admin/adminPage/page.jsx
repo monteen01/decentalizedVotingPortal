52c0301d4e9f1e9068463f807da703438f7270b0
monteen <montwork01@gmail.com>
2024-03-18 22:23:15 +0530

    added testin.txt

--- src/app/(root)/admin/adminPage/page.jsx
+++ src/app/(root)/admin/adminPage/page.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+// pages/admin.js
+"use client";
+import React, { useState } from "react";
+import votingSystemContract from "../../constants/contract";
 
-const AdminPage = () => {
-  return <div>Welcome to AdminPage</div>;
-};
+function AdminPage() {
+  const [newCandidateName, setNewCandidateName] = useState("");
+
+  const handleAddCandidate = async () => {
+    try {
+      await votingSystemContract.methods
+        .addCandidate(newCandidateName)
+        .send({ from: window.ethereum.selectedAddress });
+      setNewCandidateName("");
+      alert("Candidate added successfully!");
+    } catch (error) {
+      console.error("Error adding candidate:", error);
+      alert("Error adding candidate. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <label htmlFor="newCandidateName">New Candidate Name:</label>
+      <input
+        type="text"
+        id="newCandidateName"
+        value={newCandidateName}
+        onChange={(e) => setNewCandidateName(e.target.value)}
+        required
+      />
+      <button onClick={handleAddCandidate}>Add Candidate</button>
+    </div>
+  );
+}
 
 export default AdminPage;
